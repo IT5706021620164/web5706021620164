e4dd04da7a52a95e9efcbc0c923c47173ae8fff2
IT5706021620164 <5706021620164@fitm.kmutnb.ac.th>
2016-02-17 16:37:07 +0700

    ramdom52.php

---
+++ HWRcal.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> โปรแกรมแสดงผลลัพธ์ </title>
+  </head>
+  <body>
+  <?php
+
+    echo "<table align='center' border='4' width='300'>";
+    for($r=1 ; $r<=13 ; $r++){
+       echo "<tr>";
+       for ($c=1 ; $c<=4 ; $c++) {
+         echo "<td align='center'><font color='#33ff66'>";
+
+         }
+         echo "</tr>";
+    }
+    echo "</table>";
+   ?>
+   <a href='HWR.php'> Back </a>
+
+  </body>
+</html>
