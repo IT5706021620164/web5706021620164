de3844e32ed89282346e886983edb37e1a29c763
IT5706021620164 <5706021620164@fitm.kmutnb.ac.th>
2016-02-23 00:19:09 +0700

    Ramdom52_1

---
+++ Home/HWR.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> การสุ่มเลข </title>
+  </head>
+  <body>
+  <form method="post" action="HWRcal.php">
+  <table border="1" align="center" width="400">
+    <tr>
+      <td colspan="2" align="center">
+        <big> โปรแกรมสุ่มเลข 1-52 </big>
+      </td>
+    </tr>
+
+    <tr>
+      <td colspan="2" align="center">
+        <input type="submit" value=" OK " />
+        <input type="reset" value=" Clear " />
+      </td>
+    </tr>
+  </table>
+  </form>
+  </body>
+</html>
---
+++ Home/HWRcal.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> โปรแกรมแสดงผลลัพธ์ </title>
+  </head>
+  <body>
+  <?php
+
+        $o=1;
+    echo "<table align='center' border='4' width='300'>";
+    for($r=1 ; $r<=13 ; $r++){
+       echo "<tr>";
+       for ($c=1 ; $c<=4 ; $c++) {
+         if ($o<=52) {
+           echo "<tr><td align='center'>" . rand(1,$o) . "</td>";
+           //echo "<td align='center'>" . rand($o) . "</td>";
+         $o++;
+     }
+    }
+    echo "</tr>";
+    }
+    echo "</table>";
+   ?>
+   <a href='HWR.php'> Back </a>
+
+  </body>
+</html>
